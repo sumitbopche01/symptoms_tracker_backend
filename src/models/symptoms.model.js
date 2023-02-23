bf9b56615a91fc4f12587338f3caa6bbc2cf85a3
PoojaPanirwala <poojapanirwala24@gmail.com>
2023-02-22 20:44:27 -0500

    add avg-condition to symptom schema, Authentication changes

--- src/models/symptoms.model.js
+++ src/models/symptoms.model.js
@@ -1,18 +1,19 @@
 // load mongoose since we need it to define a model
-const mongoose = require('mongoose');
-const { number } = require('joi');
+const mongoose = require("mongoose");
+const { number } = require("joi");
 
 const { Schema } = mongoose;
 
 const SymptomScaleSchema = new Schema({
   name: { type: String },
-  scale: { type: Number }
+  scale: { type: Number },
 });
 
 const SymptomsSchema = new Schema({
   user_email: { type: String },
   symptoms: [SymptomScaleSchema],
-  date: { type: Date }
+  avg_condition: { type: Number },
+  date: { type: Date },
 });
 
-module.exports = mongoose.model('symptoms', SymptomsSchema);
+module.exports = mongoose.model("symptoms", SymptomsSchema);
--- src/routes/symptoms.route.js
+++ src/routes/symptoms.route.js
@@ -1,33 +1,34 @@
 /* eslint-disable comma-dangle */
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 
 const router = express.Router();
-const symptomsController = require('../controller/symptoms.controller');
+const symptomsController = require("../controller/symptoms.controller");
 
-/* GET restaurant  */
-router.get('/', symptomsController.getMultiple);
+/* GET Symptoms  */
+router.get("/", symptomsController.getMultiple);
 
-router.get('/:symptoms_id', symptomsController.get);
+router.get("/:symptoms_id", symptomsController.get);
 
 /* POST create restaurant */
 router.post(
-  '/',
-//  passport.authenticate('jwt', { session: false }),
+  "/",
+  //  passport.authenticate('jwt', { session: false }),
   symptomsController.create
 );
 
+
 /* PUT restaurants */
 router.put(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
   symptomsController.update
 );
 
 /* DELETE restaurants */
 router.delete(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
   symptomsController.remove
 );
 
