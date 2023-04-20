57141672bd83fae5784de9ae96411e09a6390141
Sumit Bopche <sumitbopche01@gmail.com>
2023-04-20 03:18:59 -0400

    spell changed

--- src/models/medicationTracker.model.js
+++ src/models/medicationTracker.model.js
@@ -13,7 +13,7 @@ const medicationTrackerSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    symtomSeverity: {
+    symptomSeverity: {
         type: String,
         required: true
     },
